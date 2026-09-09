2fff667538fdd7f1122e9ee0cadf7e614b938fa5
Gonzalo Viglioni <GV301042@fi365.ort.edu.uy>
2026-09-09 18:38:46 -0300

    test

--- src/models/storage.js
+++ src/models/storage.js
@@ -1,44 +1,43 @@
-const expenses = []
+const expenses = [];
+const users = [];
 let currentId = 1;
-
 
 const getExpenses = () => expenses;
 
 const findExpenses = (id) => expenses.find((e) => e.id == id);
 
 const createExpense = (concepto, costo, fecha, categoria) => {
-    const newExpense = {
-        id: currentId++,
-        concepto,
-        costo,
-        fecha,
-        categoria: categoria || null
-    }
-    expenses.push(newExpense);
-    return newExpense;
-}
-
+  const newExpense = {
+    id: currentId++,
+    concepto,
+    costo,
+    fecha,
+    categoria: categoria || null,
+  };
+  expenses.push(newExpense);
+  return newExpense;
+};
 
 const deleteExpense = (id) => {
-    let indexToBeDeleted = expenses.findIndex((e) => e.id == id);
-    if(indexToBeDeleted === -1) return false;
+  let indexToBeDeleted = expenses.findIndex((e) => e.id == id);
+  if (indexToBeDeleted === -1) return false;
 
-    expenses.splice(indexToBeDeleted, 1);
-    return true;
-}
+  expenses.splice(indexToBeDeleted, 1);
+  return true;
+};
 
 const updateExpense = (id, body) => {
-    const index = expenses.findIndex((toDo) => toDo.id == id);
-    if(index >= 0){
-        expenses[index] = {...expenses[index], ...body}
-    }
-    return expenses[index];
-}
+  const index = expenses.findIndex((toDo) => toDo.id == id);
+  if (index >= 0) {
+    expenses[index] = { ...expenses[index], ...body };
+  }
+  return expenses[index];
+};
 
 module.exports = {
-    getExpenses,
-    findExpenses,
-    createExpense,
-    deleteExpense,
-    updateExpense
-}
+  getExpenses,
+  findExpenses,
+  createExpense,
+  deleteExpense,
+  updateExpense,
+};
--- src/routes/private.router.js
+++ src/routes/private.router.js
@@ -1,23 +1,29 @@
 const express = require("express");
 const router = express.Router();
-const {
+/*const {
     getExpensesController,
     getExpenseControllerById,
     postExpenseController,
     putExpenseController,
     deleteExpenseController
-} = require("../controllers/expenses.controller");
-const payloadMiddleware = require("../middlewares/payload.middleware");
-const expensesSchema = require("../models/schemas/expense.schema")
+} = require("../controllers/expenses.controller");*/
+// const payloadMiddleware = require("../middlewares/payload.middleware");
+const expensesSchema = require("../models/schemas/expense.schema");
 
 router.get("/expenses", getExpensesController);
 
 router.get("/expenses/:id", getExpenseControllerById);
 
-router.post("/expenses", payloadMiddleware(expensesSchema), postExpenseController);
+router.post(
+  "/expenses",
+  payloadMiddleware(expensesSchema),
+  postExpenseController,
+);
 
-router.delete("/expenses/:id", deleteExpenseController)
+router.delete("/expenses/:id", deleteExpenseController);
 
 router.put("/expenses/:id", putExpenseController);
 
 module.exports = router;
+
+//test
