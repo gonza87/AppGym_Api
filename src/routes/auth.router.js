c77868543162a450bd973eddec9bef02632a80ea
Gonzalo Viglioni <gonzalo.viglioni@gmail.com>
2026-09-16 09:10:10 -0300

    validation

--- src/routes/auth.router.js
+++ src/routes/auth.router.js
@@ -2,15 +2,15 @@ const express = require("express");
 const authRouter = express.Router();
 const { postAuthLogin, postAuthSignup } = require("../controllers/auth.controller");
 const payloadMiddleWare = require("../middlewares/payload.middleware");
-const { signupSchema, loginSchema } = require("../models/schemas/auth.schema");
+const { signupValidation, loginValidation } = require("./validations/user.validation");
 
 // const {
 //   healthController,
 //   pingController,
 // } = require("../controllers/public.controller");
 
-authRouter.post("/signup", payloadMiddleWare(signupSchema) ,postAuthSignup);
-authRouter.post("/login", payloadMiddleWare(loginSchema), postAuthLogin);
+authRouter.post("/signup", payloadMiddleWare(signupValidation) ,postAuthSignup);
+authRouter.post("/login", payloadMiddleWare(loginValidation), postAuthLogin);
 
 
 module.exports = authRouter;
--- src/routes/validations/user.validation.js
+++ src/routes/validations/user.validation.js
@@ -1,18 +1,18 @@
 const Joi = require("joi");
 
-const signupSchema = Joi.object({
+const signupValidation = Joi.object({
   name: Joi.string().min(3).max(20).required(),
   username: Joi.string().min(3).max(20).required(),
   password: Joi.string().min(3).max(20).alphanum().required(),
 });
 
-const loginSchema = Joi.object({
+const loginValidation = Joi.object({
   username: Joi.string().min(3).max(20).required(),
   password: Joi.string().min(3).max(20).alphanum().required(),
 });
 
 
 module.exports = {
-    signupSchema,
-    loginSchema
+    signupValidation,
+    loginValidation
 };
