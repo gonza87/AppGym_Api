6660ac2f2ea31dd72b195a0d3b1d1b21031d6f3c
Gonzalo Viglioni <gonzalo.viglioni@gmail.com>
2026-09-16 09:49:15 -0300

    premium

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -3,25 +3,18 @@ const bcrypt = require("bcryptjs")
 const users = [
   {
     id: 1,
-    name: "Mauro",
-    username: "mnacimento",
-    password: "$2b$10$IaoymDCD.9hZn5uh1WtU4OkKQwZfSY1LGhN68gY7PC4/3GknWwUqW",
-    active: true
+    name: "Jose",
+    username: "jope",
+    password: "$2b$10$03IX8vNyeMmWbzmU11gA.Oo0ksPKtguO8t5vityc4NV3maps8p2lO",
+    premium: true
   },
     {
     id: 2,
-    name: "Emilia",
-    username: "enacimento",
-    password: "$2b$10$kBH3kt.Mkq24TYLNq1JGEuU4H5duGyPjtYwjFv8LezUvQmXn5eUT6",
-    active: false
-  },
-    {
-    id: 3,
-    name: "Martina",
-    username: "mvega",
-    password: "$2b$10$c8NGTB.tUBreCTav/Yj/kezJK.YO5JDnM7wFyBZwWkVXlDtv7EsYa",
-    active: true
-  },
+    name: "Martin",
+    username: "tincho",
+    password: "$2b$10$03IX8vNyeMmWbzmU11gA.Oo0ksPKtguO8t5vityc4NV3maps8p2lO",
+    premium: false
+  }
 ];
 
 const getUsers = () => users;
